6a8ed220aee635ae538fc964bbbce1a7329e7796
Cameron McIntosh <cam@Camerons-MacBook-Air.local>
2019-10-25 18:25:19 -0400

    Modified Sign In page

---
+++ signIn.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <h2>Sign In</h2>
+    <form method="POST" action="connect.php">
+        Email:<br>
+        <input type="email" name="email"><br>
+        Password:<br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="Sign In">
+    </form>
+
+    <p><a href="signUp.php">Sign Up</a></p>
+    </body>
+
+</html>
