16ff2b688204702356d47e01ed5412ba10bc48c4
Cameron McIntosh <cam@Camerons-MacBook-Air.local>
2019-11-08 16:45:41 -0500

    Sign Up Feature

---
+++ connect.php
@@ -0,0 +1,61 @@
+<?php
+//Sign Up info stored in variables
+$name = filter_input(INPUT_POST, 'name');
+$username = filter_input(INPUT_POST, 'username');
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
+
+if (!empty($name)) {
+    if (!empty($username)) {
+        if (!empty($email)) {
+            if (!empty($password)) { //if none of the fields are empty
+                //database info
+                $host = "localhost:3306";
+                $dbusername = "root";
+                $dbpassword = "UnderPressure301";
+                $dbname = "QuizzedIn";
+
+                //Create connection
+                $connect = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+                //If there is an error connecting to the database
+                if (mysqli_connect_error()) {
+                    die('Connect Error ('. mysqli_connect_errno() .') '
+                    . mysqli_connect_error());
+                }
+                else {
+                    //Inserts the User's Info into the 'users' table in the database
+                    $sql = "INSERT INTO users (name, username, email, password)
+                    values ('$name', '$username', '$email', '$password')";
+
+                    if ($connect->query($sql)) {
+                        echo "You have sucessfully signed up!";
+                    }
+                    else { 
+                        echo "Error: ". $sql ."
+                        ". $connect->error;
+                    }
+                    $connect->close(); //close connection
+                }
+            }
+            else {
+                echo "Password field should not be empty";
+                die();
+            }
+        }
+        else {
+            echo "Email field should not be empty";
+            die();
+        }
+        
+    }
+    else {
+        echo "Username field should not be empty";
+        die();
+    }
+}
+else {
+    echo "Name field should not be empty";
+    die();
+}
+?>
---
+++ signUp.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>QuizzedIn</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styling.css">
+    </head>
+
+
+<body>
+    
+    <form method="POST" action="connect.php">
+        Name:<br>
+        <input type="text" name="name"><br>
+        Username:<br>
+        <input type="text" name="username"><br>
+        Email:<br>
+        <input type="email" name="email"><br>
+        Password:<br>
+        <input type="password" name="password"><br>
+        Confirm Password:<br>
+        <input type="password" name="confirmPassword"><br>
+        <input type="submit" value="Submit">
+        <input type="reset" value="Reset">
+    </form>
+
+    <p><a href="signIn.php">Log In</a></p>
+
+
+
+
+
+
+
+</body>
+
+
+</html>
