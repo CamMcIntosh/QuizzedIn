179bb1a8992d4caf82aa7e7993abcfd512de9c28
Cameron McIntosh <cam@Camerons-MacBook-Air.local>
2019-11-12 12:11:17 -0500

    Added Categories

---
+++ categories.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>QuizzedIn</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styling.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="script.js"></script>
+    </head>
+
+<body>
+    <main>
+    <h1>Categories</h1>
+    <ul style="list-style-type: none;">
+        <div id="topics"></div> <!-- Categories automatically uploaded via API-->
+    </ul>
+    
+</body>
+</html>
