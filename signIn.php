4388f6ff44c05895ff660512f48109be0f175151
Cameron McIntosh <cam@Camerons-MacBook-Air.local>
2019-11-13 19:14:50 -0500

    Setup Login Functionality & Create Profile

--- signIn.php
+++ signIn.php
@@ -4,17 +4,26 @@
 
     </head>
 
+    <?php
+        include('login.php'); // Includes Login Script
+        if(isset($_SESSION['login_user'])) {
+            header("location: profile.php"); // Redirecting To Profile Page
+        }
+    ?>
+
     <body>
         <h2>Sign In</h2>
-    <form method="POST" action="connect.php">
+        <form method="POST" action="">
         Email:<br>
         <input type="email" name="email"><br>
         Password:<br>
         <input type="password" name="password"><br>
         <input type="submit" value="Sign In">
+        <span><?php echo $error; ?></span>
     </form>
 
     <p><a href="signUp.php">Sign Up</a></p>
+    
     </body>
 
 </html>
